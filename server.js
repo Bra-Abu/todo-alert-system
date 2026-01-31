44fadae47d9a6261cb5baab736e04d817eb66f77
tuffguy <clipperapp@tuffguy.com>
2026-01-31 17:00:41 +0000

    Fix: Use sameSite lax for same-origin session cookies

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    sameSite: 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
   }
 }));
